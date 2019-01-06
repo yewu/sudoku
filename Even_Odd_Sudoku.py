518bdefd8956e2510b153cd5213d093077d0dc8e
celinewu567 <46407183+celinewu567@users.noreply.github.com>
2019-01-05 20:44:32 -0500

    Update Even_Odd_Sudoku.py

--- Even_Odd_Sudoku.py
+++ Even_Odd_Sudoku.py
@@ -1,18 +1,60 @@
+import pygame
+pygame.init()
+
+white = [255,255,255]
+black = [0,0,0]
+blue = [190,210,230]
+purple = [229,194,237]
+
+bright_blue = [210,230,250]
+
+(width, height) = (1300, 1000)
+screen = pygame.display.set_mode((width, height))
+screen.fill(white)
+
+pygame.display.set_caption("Even and Odd Sudoku")
+pygame.display.update()
+
 def main():
-    import pygame
-    pygame.init()
-    black = (0,0,0)
-    grey = (220,220,220)
-    white = (255,250,250)
-    blue = (190,210,230)
-    purple = (229,194,237)
-    size = (1200, 1000)
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Even and Odd Sudoku")
     home()
 
 def home():
-    pygame.draw.rect (gameDisplay, blue, (800, 30, 300, 150))
+    mouse = pygame.mouse.get_pos() # doesn't work
+    print (mouse)
+    pygame.draw.rect(screen, blue,(900, 100, 350, 125))
+    pygame.draw.rect(screen, blue,(900, 325, 350, 125))
+    pygame.draw.rect(screen, blue,(900, 550, 350, 125))
+    pygame.draw.rect(screen, blue,(900, 775, 350, 125))
+    pygame.draw.rect(screen, purple,(100, 110, 650, 300))
+    pygame.display.update()
+
+    while 900 + 350 > mouse[0] > 900 and 100 + 125 > mouse [1] > 100:
+        pygame.draw.rect(screen, bright_blue,(900, 100, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 325, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 550, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 775, 350, 125))
+        pygame.display.update()
+
+    while 900 + 350 > mouse[0] > 900 and 325 + 125 > mouse [1] > 325:
+        pygame.draw.rect(screen, blue,(900, 100, 350, 125))
+        pygame.draw.rect(screen, bright_blue,(900, 325, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 550, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 775, 350, 125))
+        pygame.display.update()
+
+    while 900 + 350 > mouse[0] > 900 and 550 + 125 > mouse [1] > 550:
+        pygame.draw.rect(screen, blue,(900, 100, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 325, 350, 125))
+        pygame.draw.rect(screen, bright_blue,(900, 550, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 775, 350, 125))
+        pygame.display.update()
+
+    while 900 + 350 > mouse[0] > 900 and 775 + 125 > mouse [1] > 775:
+        pygame.draw.rect(screen, blue,(900, 100, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 325, 350, 125))
+        pygame.draw.rect(screen, bright_blue,(900, 550, 350, 125))
+        pygame.draw.rect(screen, blue,(900, 775, 350, 125))
+        pygame.display.update()
     
 main()
         
