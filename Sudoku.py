f3da7c41e6b59cd6cc12f8e2849dc3415c08b20c
celinewu567 <46407183+celinewu567@users.noreply.github.com>
2019-02-15 20:14:25 -0500

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -5,6 +5,7 @@ CREDITS = 0
 HOWTO = 0
 EXIT = 0
 BACK = 0
+PLAY = 0
 #it has to be 650 for the screen to fit on the size of the computer screen
 #each box will be 66x66 in the board
 
@@ -14,6 +15,7 @@ def draw():
 	global HOWTO
 	global EXIT
 	global BACK
+	global PlAY
 
 	screen.fill((255, 255, 255))
 	blue = 190, 210, 230
@@ -32,6 +34,40 @@ def draw():
 		screen.draw.filled_rect(back_button, blue)
 		screen.draw.rect(back_button, black)
 		screen.draw.text("BACK", (850, 78), color = "black", fontname = "arial", fontsize = 42)
+	
+	elif PLAY !=0:
+		screen.clear()
+		screen.fill((255,255,255))
+		heading = Rect((50,50), (650,100))
+		screen.draw.filled_rect(heading, purple)
+		screen.draw.rect(heading,(0,0,0))
+		back = Rect((750,50), (300,100))
+		screen.draw.filled_rect(back, blue)
+		screen.draw.rect(back,(0,0,0))
+		Extreme = Rect((750,200), (300,150))
+		screen.draw.filled_rect(Extreme, blue)
+		screen.draw.rect(Extreme,(0,0,0))
+		Normal = Rect((400,200), (300,150))
+		screen.draw.filled_rect(Normal, blue)
+		screen.draw.rect(Normal,(0,0,0))
+		Easy = Rect((50,200), (300,150))
+		screen.draw.filled_rect(Easy, blue)
+		screen.draw.rect(Easy,(0,0,0))
+		screen.draw.text("CHOOSE A LEVEL", (95, 67), color = "black", fontname = "arial", fontsize = 65)
+		screen.draw.text("BACK", (850, 78), color = "black", fontname = "arial", fontsize = 42)
+		screen.draw.text("Easy", (150, 210), color = "black", fontname = "arial", fontsize = 50)	
+		screen.draw.text("SOPHIA", (140, 262), color = "black", fontname = "arial", fontsize = 35)
+		screen.draw.text("(EXAMPLE BELOW)", (51, 300), color = "black", fontname = "arial", fontsize = 32)	
+		screen.draw.text("NORMAL", (450, 210), color = "black", fontname = "arial", fontsize = 50)	
+		screen.draw.text("ELINA", (508, 262), color = "black", fontname = "arial", fontsize = 35)
+		screen.draw.text("(EXAMPLE BELOW)", (401, 300), color = "black", fontname = "arial", fontsize = 32)
+		screen.draw.text("EXTREME", (780, 210), color = "black", fontname = "arial", fontsize = 50)	
+		screen.draw.text("CELINE", (834, 262), color = "black", fontname = "arial", fontsize = 35)
+		screen.draw.text("(EXAMPLE BELOW)", (751, 300), color = "black", fontname = "arial", fontsize = 32)	
+		board = Actor('easy')
+		board.pos = 200, 500
+		board.draw()	
+		board = Actor('normal')		
 	elif HOWTO != 0:
 		HOME = 3
 		screen.clear()
@@ -45,6 +81,7 @@ def draw():
 		screen.draw.filled_rect(back_button, blue)
 		screen.draw.rect(back_button, black)
 		screen.draw.text("BACK", (850, 78), color = "black", fontname = "arial", fontsize = 42)
+	
 	elif BACK != 0 or CREDITS == 0 or HOWTO == 0:
 		HOME = 1
 		screen.clear()
@@ -71,7 +108,7 @@ def draw():
 		board.pos = 350, 450
 		board.draw()
 		#text on butttons and title
-		screen.draw.text("PLAY", (850, 78), color = "black", fontname = "arial", fontsize = 42)
+		screen.draw.text("PLAY", (845, 78), color = "black", fontname = "arial", fontsize = 42)
 		screen.draw.text("HOW TO PLAY", (755, 225), color = "black", fontname = "arial", fontsize = 42)
 		screen.draw.text("CREDITS", (806, 375), color = "black", fontname = "arial", fontsize = 42)
 		screen.draw.text("EXIT", (858, 525), color = "black", fontname = "arial", fontsize = 42)
@@ -92,8 +129,9 @@ def on_mouse_down(pos):
 	global HOWTO
 	global EXIT
 	global BACK
+	global PLAY
 	if pos[0]> 750 and pos[0] < 1050 and pos[1] > 50 and pos[1] < 150 and HOME == 1:
-		print ("play button clicked")
+		PLAY = 1
 	if pos[0]> 750 and pos[0] < 1050 and pos[1] > 50 and pos[1] < 150 and HOME == 2:
 		BACK = 1
 		CREDITS = 0
